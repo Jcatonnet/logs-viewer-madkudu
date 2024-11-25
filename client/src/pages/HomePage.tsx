16a4795b33d4e2081db0d07796def7ed76930a9e
Julien Catonnet <julien.catonnet@essec.edu>
2024-11-25 00:16:17 +0000

    fix: update toast in homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import FileUpload from '../components/FileUpload';
 import LogsList from '../components/LogsList';
+import CustomToast from '../components/Toast';
 
 const HomePage: React.FC = () => {
     const [refreshLogs, setRefreshLogs] = useState<boolean>(false);
+    const [showToast, setShowToast] = useState(false);
+    const [toastMessage, setToastMessage] = useState('');
+
     const handleUploadSuccess = () => {
         setRefreshLogs((prev) => !prev);
+    };
+
+    const handleShowToast = (message: string) => {
+        setToastMessage(message);
+        setShowToast(true);
     };
 
     return (
         <div>
             <h1>Welcome to the Log Viewer Application</h1>
-            <FileUpload onUploadSuccess={handleUploadSuccess} />
+            <FileUpload onShowToast={handleShowToast}
+                onUploadSuccess={handleUploadSuccess} />
             <LogsList refreshLogs={refreshLogs} />
+            <CustomToast
+                show={showToast}
+                message={toastMessage}
+                onClose={() => setShowToast(false)}
+            />
         </div>
     );
 };
